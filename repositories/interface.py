d082c0ac6034a6bd07ddb30616ce0df37c79fdfd
manuellereis <mrsantana0227@gmail.com>
2024-11-23 16:09:51 -0500

    New commit

--- repositories/interface.py
+++
@@ -1,8 +0,0 @@
-
-from abc import ABC, abstractmethod
-import typing as t
-
-class Repository(ABC):
-    @abstractmethod
-    def get(self, *args, **kargs) -> t.Any:
-        raise NotImplementedError
